609b02300ee66e39182269d48ff52569ff510e82
Swarnim Dighe <101547512+swarnim14@users.noreply.github.com>
2024-06-08 18:38:46 +0530

    Time: 1 ms (76.87%) | Memory: 45.6 MB (35.72%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,45 +1,75 @@
 public class Solution {
-    public void setZeroes(int[][] matrix){
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        boolean isRow0 = false, isCol0 = false;
 
-		int m=matrix.length, n=matrix[0].length;
-        boolean isRow0=false, isCol0=false;
-        
-        for(int j=0;j<n;j++){
-            if(matrix[0][j]==0)
-                isRow0=true;
+        // Check if first row has any zeros
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                isRow0 = true;
+                break;
+            }
         }
-        
-        for(int i=0;i<m;i++){
-            if(matrix[i][0]==0)
-                isCol0=true;
+
+        // Check if first column has any zeros
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                isCol0 = true;
+                break;
+            }
         }
-        
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                if(matrix[i][j]==0){
-                    matrix[i][0]=0;
-                    matrix[0][j]=0;
+
+        // Mark zeros in the first row and column
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-        
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                if(matrix[0][j]==0 || matrix[i][0]==0)
-                    matrix[i][j]=0;
+
+        // Use marks to set elements to zero
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+                    matrix[i][j] = 0;
+                }
             }
         }
-        
-        if(isRow0){
-            for(int j=0;j<n;j++)
-                matrix[0][j]=0;
+
+        // Update first row if needed
+        if (isRow0) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
         }
-        
-        if(isCol0){
-            for(int i=0;i<m;i++)
-                matrix[i][0]=0;
+
+        // Update first column if needed
+        if (isCol0) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
         }
     }
-   
-}
 
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[][] matrix = {
+            {1, 1, 1},
+            {1, 0, 1},
+            {1, 1, 1}
+        };
+
+        solution.setZeroes(matrix);
+
+        // Print the matrix to verify the result
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
