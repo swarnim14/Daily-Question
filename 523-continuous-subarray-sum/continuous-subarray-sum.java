de79ee5f61d988d50e8b42adaff22970897e599d
Swarnim Dighe <101547512+swarnim14@users.noreply.github.com>
2024-06-08 12:00:37 +0530

    Time: 20 ms (92.02%) | Memory: 57.4 MB (69.25%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.java
+++ 523-continuous-subarray-sum/continuous-subarray-sum.java
@@ -1,22 +1,38 @@
+import java.util.HashMap;
+
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
-     HashMap<Integer,Integer> map = new HashMap <>();
-     map.put(0,-1);
-     int sum = 0;
-     for(int i= 0 ; i<nums.length;i++){
-        sum+=nums[i];
-        if(map.containsKey(sum%k)){
-            int index =map.get(sum%k);
-            if(i-index>=2){
-                return true;
+        // Handle edge case where k is zero
+        if (k == 0) {
+            // Check if there are at least two consecutive zeros
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] == 0 && nums[i - 1] == 0) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        // Normalize k to positive
+        k = Math.abs(k);
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1); // To handle the case where the sum itself is a multiple of k
+
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            int mod = sum % k;
+
+            if (map.containsKey(mod)) {
+                int index = map.get(mod);
+                if (i - index >= 2) {
+                    return true;
+                }
+            } else {
+                map.put(mod, i); // Store first occurrence
             }
         }
-        else{
-            map.put(sum%k,i);//store first occurence 
-        }
-      
-        
-    }
-      return false;
+        return false;
     }
 }
